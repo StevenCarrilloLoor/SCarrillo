e741b3cc8f824bb6a96287a0a9c1005c4cd1b7ac
StevenCarrilloLoor <stevencarrilloloor@gmail.com>
2024-05-02 08:19:19 -0500

    Correccion de la generacion del frame

---
+++ SCarrillo/Migrations/20240502131632_InitialCreate.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SCarrillo.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Carrera",
+                columns: table => new
+                {
+                    CarreraId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreCarrera = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Campus = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    NumeroSemestres = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carrera", x => x.CarreraId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SCarrillo",
+                columns: table => new
+                {
+                    EstudianteId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Edad = table.Column<int>(type: "int", nullable: false),
+                    Promedio = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Activo = table.Column<bool>(type: "bit", nullable: false),
+                    FechaIngreso = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CarreraId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SCarrillo", x => x.EstudianteId);
+                    table.ForeignKey(
+                        name: "FK_SCarrillo_Carrera_CarreraId",
+                        column: x => x.CarreraId,
+                        principalTable: "Carrera",
+                        principalColumn: "CarreraId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SCarrillo_CarreraId",
+                table: "SCarrillo",
+                column: "CarreraId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SCarrillo");
+
+            migrationBuilder.DropTable(
+                name: "Carrera");
+        }
+    }
+}
