7eea893487d28ca59d0e565be43eff07a5005104
steve <steve@STEVEN_ASUS>
2024-05-02 07:41:03 -0500

    Agregar archivos de proyecto.

---
+++ SCarrillo/Models/Carrera.cs
@@ -0,0 +1,11 @@
+﻿namespace SCarrillo.Models
+{
+    public class Carrera
+    {
+        public int Id { get; set; }
+        public string nombre_Carrera { get; set; }
+        public string campus { get; set; }
+        public int numero_Carrera { get; set; }
+
+    }
+}
---
+++ SCarrillo/Models/SCarrillo.cs
@@ -0,0 +1,12 @@
+﻿namespace SCarrillo.Models
+{
+    public class SCarrillo
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public bool beca {  get; set; }
+        public DateTime fecha { get; set; }
+
+
+    }
+}
